b7146074cb970110acb6dca8e683e8367f89cb28
CeciliaHermo <florfeliz007@gmail.com>
2024-07-18 10:09:45 -0600

    Atque y mascota enemigo

--- JS/pokemon.js
+++ JS/pokemon.js
@@ -1,5 +1,6 @@
  let ataqueJugador
- 
+ let ataqueEnemigo
+
  function iniciarJuego(){
     let botonMascotaJugador = docment.getElementById("boton-mascota")
     botonMascotaJugador.addEvenListener("click", seleccionarMascotaJugador)
@@ -18,7 +19,7 @@
   }
 
   function seleccionarMascotaJugador(){
-    let seleccionarMascota =docment.getElementById("seleccionar-mascota")
+    let seccionSeleccionarMascota =docment.getElementById("seleccionar-mascota")
     seleccionarMascota.style.display = "none" 
 
     let seccionSeleccionAtaque = document.getElementById("seleccionar-ataque")
@@ -26,9 +27,11 @@
 
     let inputCharmander = document.getElementById("charmander")
 
-    let inputBulbasar = document.getElementById("bulbasaur")
+    let inputBulbasaur = document.getElementById("bulbasaur")
 
     let inputSquirtle = document.getElementById("squirtle")
+
+    let spanMascota = document.getElementById("mascota-jugador")
 
     if(inputCharmander.checked){
       spanMascotaEnemigo.innerhtml = "charmander"
@@ -42,17 +45,64 @@
       spanMascotaEnemigo.innerhtml = "squirtle"
     } else{
       alert("Seleccionar una mascota")
-    }
+    } 
+    
+    seleccionarMascotaEnemigo()
+  }
+
+  function seleccionarMascotaEnemigo(){
+    let ataqueAleatorio = aleatorio(1,3) 
+    let spanMascotaEnemigo = docment.getElementById ="mascota-enemigo"
+
+    if(ataqueAleatorio==1){
+      spanMascotaEnemigo.innerhtml = "charmander"
+    } else
+
+    if(ataqueAleatorio==2){
+      spanMascotaEnemigo.innerhtml = "bulbasaur"
+    } else
+
+    if(ataqueAleatorio==3){
+      spanMascotaEnemigo.innerhtml = "squirtle"
+    } 
+    
   }
 
   function ataqueFuego(){
     ataqueJugador = "fuego"
+    ataqueAleatorioEnemigo()
   }
 
   function ataqueAgua(){
     ataqueJugador = "agua"
+    ataqueAleatorioEnemigo()
   }
 
   function ataqueTierra(){
     ataqueJugador = "tierra"
+    ataqueAleatorioEnemigo()
   }
+
+  function ataqueAleatorioEnemigo(){
+   let ataqueAleatorio = aleatorio(1,3)
+
+   if (ataqueAleatorio == 1){
+    ataqueEnemigo = "fuego"
+   }else
+
+   if (ataqueAleatorio == 2){
+    ataqueEnemigo = "agua"
+   }else
+
+   if (ataqueAleatorio == 3){
+    ataqueEnemigo = "tierra"
+   }
+
+  
+  }
+
+  function aleatorio(min,max){
+    return math.floor(math.random()*(max-min+1)+min)
+  }
+
+  window.addEventListener("load", iniciarJuego)
